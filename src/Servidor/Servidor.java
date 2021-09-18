1000e9286038f96d3c5080e30e5b49672ceb09f5
UnfriendlyKoala <p.vallejo@uniandes.edu.co>
2021-09-18 07:32:37 -0500

    Base servidor terminada

--- src/Servidor/Servidor.java
+++ src/Servidor/Servidor.java
@@ -3,6 +3,12 @@
  */
 package Servidor;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author S2G11
  *
@@ -16,8 +22,32 @@ public class Servidor {
 	// MAIN
 	//----------------------------------------
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ServerSocket servidor = null;
+		Socket sc = null;
+		DataInputStream in;
+		DataOutputStream out;
+		final int PUERTO = 5000;
+		
+		try {
+			servidor = new ServerSocket(PUERTO);
+			System.out.println("Servidor iniciado");
+			while (true) {
+				sc = servidor.accept();
+				
+				in = new DataInputStream(sc.getInputStream());
+				out = new DataOutputStream(sc.getOutputStream());
+				
+				String mensaje = in.readUTF();
+				System.out.println(mensaje);
+				
+				out.writeUTF("Servidor");
+				
+				sc.close();
+				System.out.println("Cliente desconectado");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
