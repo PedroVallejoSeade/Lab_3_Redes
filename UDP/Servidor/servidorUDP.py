d3ded3d713e6553b2a2147f39ddc34f66b2b4722
UnfriendlyKoala <p.vallejo@uniandes.edu.co>
2021-10-17 17:27:59 -0500

    Requerimientos cliente UDP terminados

--- UDP/Servidor/servidorUDP.py
+++ UDP/Servidor/servidorUDP.py
@@ -1,9 +1,11 @@
 import socket
 import threading
+import os
+import hashlib
 
 
-def manejadorCliente(servidor, addr):
-    with open("ArchivosEnvio/100MB.txt", "r") as f:
+def manejadorCliente(servidor, addr, nomArchivo):
+    with open(nomArchivo, "r") as f:
         while True:
             data = f.read(1024)
             data = data.encode('utf-8')
@@ -32,16 +34,40 @@ if __name__ == '__main__':
         numConexiones = input("[+] Ingrese el numero de clientes (1-25):")
         if(int(numConexiones) >= 1 and int(numConexiones) <= 25):
             numConexionesVal = True
+            print(f'[+] Se escogio total de {numConexiones} conexiones')
         else:
             print(f"[+] {numConexiones} no es un numero valido de conexiones")
 
     numConexionesReq = int(numConexiones)
+
+    archivoValido = False
+    nomArchivo = ""
+    tamArchivo = 0
+    while (not archivoValido):
+        archivo = input("[+] Escoja entre el archivo 1(100MB) o 2(250MB):")
+        if(int(archivo) == 2):
+            nomArchivo = "ArchivosEnvio/250MB.txt"
+            tamArchivo = os.path.getsize(nomArchivo)
+            archivoValido = True
+        elif(int(archivo) == 1):
+            nomArchivo = "ArchivosEnvio/100MB.txt"
+            tamArchivo = os.path.getsize(nomArchivo)
+            archivoValido = True
+        else:
+            print("El archivo escogido no es valido")
+        print(f'[+] Se escogio el archivo: {nomArchivo}')
+
     print(
-        f'[+] Se necesitan {numConexiones} para empezar la transferencia de archivos')
+        f'[+] Se necesitan {numConexiones} conexiones para empezar la transferencia de archivos')
 
-    # TODO Se necesita hacer la parte de escoger un archivo
-
-    # TODO Se necesita hacer la parte de hacer el hashing del archivo
+    md5 = hashlib.md5()
+    with open(nomArchivo, 'rb') as f:
+        while True:
+            data = f.read(1024)
+            if not data:
+                break
+            md5.update(data)
+    hashServidor = md5.hexdigest()
 
     hilos = []
 
@@ -50,27 +76,28 @@ if __name__ == '__main__':
         data = data.decode("utf-8")
         print(data)
         numConexionesAct += 1
-        print(f"[+] Inicializacion con el cliente {numConexionesAct} exitosa")
+
+        data = f'{numConexionesAct}_{numConexionesReq}_{nomArchivo}_{tamArchivo}_{hashServidor}'
+        data = data.encode("utf-8")
+        server.sendto(data, addr)
 
         data = '[-] Inicializacion con el servidor exitosa'
         data = data.encode("utf-8")
         server.sendto(data, addr)
 
-        hilo = threading.Thread(target=manejadorCliente, args=(server, addr))
+        hilo = threading.Thread(target=manejadorCliente,
+                                args=(server, addr, nomArchivo))
         hilos.append(hilo)
+
+        print(f"[+] Inicializacion con el cliente {numConexionesAct} exitosa")
+        if(numConexionesReq - numConexionesAct == 0):
+            print(
+                f'[+] Comienza la transferencia de archivos')
+        else:
+            print(
+                f'[+] Se necesitan {numConexionesReq-numConexionesAct} conexiones para empezar la transferencia de archivos')
 
     i = 0
     while (i < numConexionesAct):
         hilos[i].start()
         i += 1
-
-    # with open("ArchivosEnvio/100MB.txt", "r") as f:
-    #     while True:
-    #         data = f.read(1024)
-    #         data = data.encode('utf-8')
-
-    #         if not data:
-    #             server.sendto(data, addr)
-    #             break
-
-    #         server.sendto(data, addr)
