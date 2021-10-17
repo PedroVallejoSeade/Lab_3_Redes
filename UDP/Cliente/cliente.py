f7883a3a7770ac108439861c44e172b63a3197da
UnfriendlyKoala <p.vallejo@uniandes.edu.co>
2021-10-17 18:29:05 -0500

    UDP terminado

--- UDP/Cliente/cliente.py
+++ UDP/Cliente/cliente.py
@@ -1,110 +1,91 @@
 import socket
-import logging
 from datetime import datetime
+import logging
+import os
 import hashlib
 
-IP = socket.gethostbyname(socket.gethostname())
-PUERTO = 8000
-DIRECCION = (IP, PUERTO)
-TAMANIO = 1024
-FORMATO = "utf-8"
-
-
-def main():
+if __name__ == '__main__':
     dateTimeObj = datetime.now()
     logging.basicConfig(
         filename=f"Logs/{dateTimeObj.year}-{dateTimeObj.month}-{dateTimeObj.day}-{dateTimeObj.hour}-{dateTimeObj.minute}-{dateTimeObj.second}.log", level=logging.INFO)
 
-    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    cliente.connect(DIRECCION)
-
-    data = cliente.recv(TAMANIO).decode('utf-8')
-    item = data.split("_")
-    NOM_ARCHIVO = item[0]
-    TAM_ARCHIVO = int(item[1])
-    ID = item[2]
-    NUM_CONEXIONES = item[3]
-    hashServidor = item[4]
-    ip = item[5]
-    puerto = item[6]
-
-    logging.info(
-        f"El nombre del archivo recivido es: {NOM_ARCHIVO} con un tamaño de {TAM_ARCHIVO}")
-
-    logging.info(
-        f"Este es el cliente {ID} conectado desde la siguiente direccion por TCP: {ip} : {puerto}")
-
-    print("[+] Nombre y tamanio del archivo recivido del servidor")
-    cliente.send("Nombre y tamanio del archivo recivido".encode(FORMATO))
-
-    # UDP
     host = socket.gethostbyname(socket.gethostname())
     port = 4455
     addr = (host, port)
 
-    clienteUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     data = '[-] Conexion UDP con cliente exitosa'
     data = data.encode('utf8')
-    clienteUDP.sendto(data, addr)
-    print('HOLA')
+    client.sendto(data, addr)
 
-    data, addr = clienteUDP.recvfrom(1024)
+    data, addr = client.recvfrom(1024)
+    data = data.decode('utf8')
+    item = data.split("_")
+    numCliente = item[0]
+    cantConexiones = item[1]
+    nomArchivo = item[2].split("/")[1]
+    tamArchivo = item[3]
+    hashServidor = item[4]
+
+    data, addr = client.recvfrom(1024)
     data = data.decode('utf8')
     print(data)
 
-    numPaquetesRecividos = 0
+    print(
+        f'[+] El servidor asigno el numero de cliente {numCliente} de un total de {cantConexiones} clientes')
+    print(
+        f'[+] Se recibira el archivo "{nomArchivo}" que tiene un tamanio de {tamArchivo} bytes')
+    print(
+        f'[+] Se recibe el siguiente valor de hash del servidor: {hashServidor}')
+    logging.info(f'Numero de cliente asignado: {numCliente}')
+    logging.info(f'Numero total de clientes: {cantConexiones}')
+    logging.info(f'El nombre del archivo a recibir es: {nomArchivo}')
+    logging.info(f'El tamanio del archivo a recibir es de: {tamArchivo} bytes')
+
+    numPaquetesRecibidos = 0
     tiempoTranferenciaI = datetime.now()
-    with open(f"ArchivosRecibidos/{ID}-Prueba-{NUM_CONEXIONES}.txt", "w") as f:
+    with open(f"ArchivosRecibidos/{numCliente}–Prueba-{cantConexiones}.txt", "w") as f:
         while True:
-            data, addr = clienteUDP.recvfrom(1024)
+            data, addr = client.recvfrom(1024)
+            if(numPaquetesRecibidos == 0):
+                tiempoTranferenciaI = datetime.now()
+                print('[+] Comienza la transferencia del archivo')
 
             if not data:
                 break
 
             data = data.decode('utf8')
             f.write(data)
-            numPaquetesRecividos += 1
+            numPaquetesRecibidos += 1
     tiempoTranferenciaF = datetime.now()
-    clienteUDP.close()
-
+    print('[+] Termina la transferencia del archivo')
     tiempoTranferencia = tiempoTranferenciaF-tiempoTranferenciaI
-
-    ## HASHING ##
-    BUF_SIZE = 1024
+    tamArchivoRecibido = os.path.getsize(
+        f"ArchivosRecibidos/{numCliente}–Prueba-{cantConexiones}.txt")
 
     md5 = hashlib.md5()
-
-    with open(f"ArchivosRecibidos/{ID}-Prueba-{NUM_CONEXIONES}", 'rb') as f:
+    with open(f"ArchivosRecibidos/{numCliente}–Prueba-{cantConexiones}.txt", 'rb') as f:
         while True:
-            data = f.read(BUF_SIZE)
+            data = f.read(1024)
             if not data:
                 break
             md5.update(data)
-
     hashCliente = md5.hexdigest()
+    entregaArchivoExitosa = True if hashCliente == hashServidor else False
+    print(
+        f'[+] Se genero el siguiente valor de hash localmente: {hashCliente}')
 
-    ## HASHING ##
-    if(hashCliente == hashServidor):
-        print(
-            "[+] Se comparo los hashing del servidor y del cliente y el archivo llego correctamente")
-        cliente.send('ENVIO EXITOSO'.encode(FORMATO))
-        logging.info(
-            'La entrega del archivo fue ecxitosa [Los hashings del cliente y del servidor coinciden]')
-    else:
-        print("[+] Se comparo los hashing del servidor y del cliente y el archivo NO llego correctamente")
-        cliente.send('ENVIO FALLIDO'.encode(FORMATO))
-        logging.info(
-            'La entrega del archivo NO fue ecxitosa [Los hashings del cliente y del servidor NO coinciden]')
+    data = f'{numCliente}_{entregaArchivoExitosa}'
+    data = data.encode('utf8')
+    client.sendto(data, addr)
+    client.close()
 
-    logging.info(f'El tiempo de transferencia fue de: {tiempoTranferencia}')
     logging.info(
-        f'El numero de paquetes recibidos fue: {numPaquetesRecividos}')
+        f'Al comparar los hash del archivo enviado y el archivo recivido, se concluye que se recibio el archivo correctamente: {entregaArchivoExitosa}')
     logging.info(
-        f"El numero de bytes recibidos fue: {numPaquetesRecividos*TAMANIO}")
-
-    cliente.close()
-
-
-if __name__ == "__main__":
-    main()
+        f'El tiempo de transferencia fue: {tiempoTranferencia}')
+    logging.info(
+        f'Numero de paquetes recibidos: {numPaquetesRecibidos}')
+    logging.info(
+        f'Numero de bytes recibidos: {tamArchivoRecibido}')
--- UDP/Servidor/servidor.py
+++ UDP/Servidor/servidor.py
@@ -1,145 +1,133 @@
 import socket
 import threading
 import os
+import hashlib
 import logging
 from datetime import datetime
-import hashlib
-
-IP = socket.gethostbyname(socket.gethostname())
-PUERTO = 8000
-DIRECCION = (IP, PUERTO)
-TAMANIO = 1024
-FORMATO = "utf-8"
 
 
-def manejarCliente(conn, addr, NOM_ARCHIVO, TAM_ARCHIVO, ID, NUM_CONEXIONES, hashServidor, ip, puerto, servidorUDP):
-    logging.info(f'Se conecto el cliente {ID} desde {ip}:{puerto}')
-
-    print("[+] Nueva conexion {addr}: conectado")
-
-    data = f"{NOM_ARCHIVO}_{TAM_ARCHIVO}_{ID}_{NUM_CONEXIONES}_{hashServidor}_{ip}_{puerto}"
-    conn.send(data.encode(FORMATO))
-    mensaje = conn.recv(TAMANIO).decode(FORMATO)
-    print("[-] El cliente responde: " + mensaje)
-
-    data, addr = servidorUDP.recvfrom(1024)
-    data = data.decode("utf-8")
-    print(data)
-
-    print(servidorUDP)
-    data = f'[-] Conexion con el servidor UDP exitosa'
-    data = data.encode("utf-8")
-    servidorUDP.sendto(data, addr)
-
+def manejadorCliente(servidor, addr, nomArchivo, numCliente, tamArchivo):
     numPaquetesEnviados = 0
+    print(f'[+] Comienza la transferencia del archivo al cliente {numCliente}')
     tiempoTranferenciaI = datetime.now()
-    with open(NOM_ARCHIVO, "r") as f:
+    with open(nomArchivo, "r") as f:
         while True:
             data = f.read(1024)
             data = data.encode('utf-8')
 
             if not data:
-                servidorUDP.sendto(data, addr)
+                servidor.sendto(data, addr)
                 break
 
-            servidorUDP.sendto(data, addr)
+            server.sendto(data, addr)
             numPaquetesEnviados += 1
     tiempoTranferenciaF = datetime.now()
-
+    print(f'[+] Termina la transferencia del archivo al cliente {numCliente}')
     tiempoTranferencia = tiempoTranferenciaF-tiempoTranferenciaI
-    logging.info(
-        f'El tiempo de transferencia del cliente {ID} fue de: {tiempoTranferencia}')
 
+    data, addr = server.recvfrom(1024)
+    data = data.decode("utf-8")
+    item = data.split("_")
     logging.info(
-        f'El numero de paquetes enviado al cliente {ID} fue: {numPaquetesEnviados}')
+        f'La recepcion del archivo por parte del cliente {item[0]} fue exitosa: {item[1]}')
     logging.info(
-        f'El numero de bytes enviado al cliente {ID} fue: {numPaquetesEnviados*TAMANIO}')
-    conn.close()
+        f'El tiempo de transferencia del archivo al cliente {numCliente} fue de: {tiempoTranferencia}')
+    logging.info(
+        f'El numero de paquetes enviado al cliente {numCliente} fue: {numPaquetesEnviados}')
+    logging.info(f'Se envio un total de: {tamArchivo} bytes')
 
 
-def main():
+if __name__ == '__main__':
     dateTimeObj = datetime.now()
     logging.basicConfig(
         filename=f"Logs/{dateTimeObj.year}-{dateTimeObj.month}-{dateTimeObj.day}-{dateTimeObj.hour}-{dateTimeObj.minute}-{dateTimeObj.second}.log", level=logging.INFO)
 
-    numValidoConexiones = False
-    while(not numValidoConexiones):
-        numConexiones = input("Numero de conexiones (1-25):")
-        if(int(numConexiones) >= 1 and int(numConexiones) <= 25):
-            numValidoConexiones = True
-        else:
-            print(f"{numConexiones} no es un numero valido de conexiones")
+    host = socket.gethostbyname(socket.gethostname())
+    port = 4455
 
-    NUM_CONEXIONES = int(numConexiones)
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    server.bind((host, port))
+    print('[+] Servidor UDP inicializado')
+
+    numConexionesAct = 0
+    numConexionesReq = 0
+    numConexionesVal = False
+
+    while(not numConexionesVal):
+        numConexiones = input("[+] Ingrese el numero de clientes (1-25):")
+        if(int(numConexiones) >= 1 and int(numConexiones) <= 25):
+            numConexionesVal = True
+            print(f'[+] Se escogio total de {numConexiones} conexiones')
+        else:
+            print(f"[+] {numConexiones} no es un numero valido de conexiones")
+
+    numConexionesReq = int(numConexiones)
 
     archivoValido = False
-    while(not archivoValido):
-        archivo = input("Escoja entre el archivo 1(100MB) o 2(250MB):")
+    nomArchivo = ""
+    tamArchivo = 0
+    while (not archivoValido):
+        archivo = input("[+] Escoja entre el archivo 1(100MB) o 2(250MB):")
         if(int(archivo) == 2):
-            NOM_ARCHIVO = "ArchivosEnvio/250MB.txt"
-            TAM_ARCHIVO = os.path.getsize(NOM_ARCHIVO)
+            nomArchivo = "ArchivosEnvio/250MB.txt"
+            tamArchivo = os.path.getsize(nomArchivo)
             archivoValido = True
         elif(int(archivo) == 1):
-            NOM_ARCHIVO = "ArchivosEnvio/100MB.txt"
-            TAM_ARCHIVO = os.path.getsize(NOM_ARCHIVO)
+            nomArchivo = "ArchivosEnvio/100MB.txt"
+            tamArchivo = os.path.getsize(nomArchivo)
             archivoValido = True
         else:
             print("El archivo escogido no es valido")
+        print(f'[+] Se escogio el archivo: {nomArchivo}')
 
-    ## HASHING ##
-    BUF_SIZE = 1024
+    logging.info(f'Nombre del archivo enviado: {nomArchivo}')
+    logging.info(f'Tamanio del archivo enviado: {tamArchivo} bytes')
+
+    print(
+        f'[+] Se necesitan {numConexiones} conexiones para empezar la transferencia de archivos')
 
     md5 = hashlib.md5()
-
-    with open(NOM_ARCHIVO, 'rb') as f:
+    with open(nomArchivo, 'rb') as f:
         while True:
-            data = f.read(BUF_SIZE)
+            data = f.read(1024)
             if not data:
                 break
             md5.update(data)
-
     hashServidor = md5.hexdigest()
 
-    ## HASHING ##
+    hilos = []
 
-    logging.info(f'El nombre del archivo envidado : {NOM_ARCHIVO}')
-    logging.info(f'El tamanio del archivo enviado es : {TAM_ARCHIVO}')
+    while(numConexionesAct < numConexionesReq):
+        data, addr = server.recvfrom(1024)
+        data = data.decode("utf-8")
+        print(data)
+        numConexionesAct += 1
 
-    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    servidor.bind(DIRECCION)
-    servidor.listen()
-    print("[+] Servidor escuchando ....")
+        data = f'{numConexionesAct}_{numConexionesReq}_{nomArchivo}_{tamArchivo}_{hashServidor}'
+        data = data.encode("utf-8")
+        server.sendto(data, addr)
 
-    threads = []
+        data = '[-] Inicializacion con el servidor exitosa'
+        data = data.encode("utf-8")
+        server.sendto(data, addr)
 
-    NUM_CLIENTES = 0
-    while (NUM_CLIENTES < NUM_CONEXIONES):
-        # SERVIDOR UDP (INICIO)
-        hostUDP = socket.gethostbyname(socket.gethostname())
-        portUDP = 4455
+        hilo = threading.Thread(target=manejadorCliente,
+                                args=(server, addr, nomArchivo, numConexionesAct, tamArchivo))
+        hilos.append(hilo)
 
-        serverUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        logging.info(
+            f'El cliente {numConexionesAct} se encuentra en la siguiente direccion: {addr}')
 
-        serverUDP.bind((hostUDP, portUDP))
-        # SERVIDOR UDP (FIN)
-
-        conn, addr = servidor.accept()
-        NUM_CLIENTES += 1
-        print(f"[+] Cliente conectado desde {addr[0]}:{addr[1]}")
-        thread = threading.Thread(target=manejarCliente, args=(
-            conn, addr, NOM_ARCHIVO, TAM_ARCHIVO, NUM_CLIENTES, NUM_CONEXIONES, hashServidor, addr[0], {addr[1]}, serverUDP))
-        threads.append(thread)
-        print(f"[+] Threads activos: {threading.active_count()-1}")
-        print(f"[+] Clientes conectados: {NUM_CLIENTES}")
+        print(f"[+] Inicializacion con el cliente {numConexionesAct} exitosa")
+        if(numConexionesReq - numConexionesAct == 0):
+            print(
+                f'[+] Comienza la transferencia de archivos')
+        else:
+            print(
+                f'[+] Se necesitan {numConexionesReq-numConexionesAct} conexiones para empezar la transferencia de archivos')
 
     i = 0
-    while(i < NUM_CLIENTES):
-        threads[i].start()
+    while (i < numConexionesAct):
+        hilos[i].start()
         i += 1
-
-    servidor.close()
-    serverUDP.close()
-
-
-if __name__ == "__main__":
-    main()
